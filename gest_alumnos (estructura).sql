c47cac11252edcdb103dfa832dc216f0571bc57f
juliomajara <julipi.sf@gmail.com>
2026-02-20 08:55:39 +0100

    cambios

--- gest_alumnos (estructura).sql
+++ gest_alumnos (estructura).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-02-2026 a las 18:27:45
+-- Tiempo de generación: 20-02-2026 a las 08:55:10
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
